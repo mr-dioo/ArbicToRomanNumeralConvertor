71fcf2b9066606a8ef56e71905531502be71e87f
mr-dioo <63169446+mr-dioo@users.noreply.github.com>
2024-03-15 15:02:50 +0000

    debug the recursive algorithms. change error from id identifier to class

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const inputNumber = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn")
 const output = document.getElementById("output");
-const errorBox = document.getElementById("error");
 
 const romanLetters = [
   { value: 1000, letter: 'M'  },
@@ -9,9 +8,9 @@ const romanLetters = [
   { value: 500,  letter: 'D'  },
   { value: 400,  letter: 'CD' },
   { value: 100,  letter: 'C'  },
-  { value: 90,   letter: 'LC' },
+  { value: 90,   letter: 'XC' },
   { value: 50,   letter: 'L'  },
-  { value: 49,   letter: 'XL' },
+  { value: 40,   letter: 'XL' },
   { value: 10,   letter: 'X'  },
   { value: 9,    letter: 'IX' },
   { value: 5,    letter: 'V'  },
@@ -25,50 +24,49 @@ const checkUserInput = () => {
   const inputInt = parseInt(inputNumber.value) 
   if (!inputNumber.value || isNaN(inputInt)) {
     notifyInvalidInput("invalidNumber");
-    return
+    
   }else if (inputInt >= 4000) {
-    notifyInvalidNumber("bigNumber")
-    return
+    notifyInvalidInput("bigNumber")
   } else if(inputInt < 1){
-    notifyInvalidNumber("nonPositiveNumber")
+    notifyInvalidInput("nonPositiveNumber")
+  } else {
+    output.innerText = ArabicToRoman(inputInt , 0);
   }
-  output.innerHTML = ArabicToRoman(inputInt);
   output.classList.toggle("hidden")
 }
 
 const hidePreviousOutput = () => {
   output.classList.contains("hidden") || output.classList.toggle("hidden");
-  errorBox.classList.contains("hidden") || errorBox.classList.toggle("hidden");
+  output.classList.contains("error") && output.classList.toggle("error");
 }
 
-const notifyInvalidNumber = (reason) => {
+const notifyInvalidInput = (reason) => {
   switch (reason) {
     case 'invalidNumber':
-      errorBox.innerText = 'Please enter a valid number';
+      output.innerText = 'Please enter a valid number';
       break;
     case 'bigNumber':
-      errorBox.innerText = 'Please enter a number less than or equal to 3999';
+      output.innerText = 'Please enter a number less than or equal to 3999';
       break;
     case 'nonPositiveNumber':
-      errorBox.innerText = 'Please enter a number greater than or equal to 1';
+      output.innerText = 'Please enter a number greater than or equal to 1';
       break;
   }
-  errorBox.classList.toggle("hidden");
+  output.classList.toggle("error");
 }
 
-const ArabicToRoman = (i) => {
+const ArabicToRoman = (input , LetterIndex) => {
   
-  if (i === 1) {
-    return `I`;
+  if (input === 0) {
+    return "";
   }
-  
-  for (l of romanLetters) {
-    if (i > l.value) {
-      console.log(`remiander: ${i} , letter: ${l.letter} `)
-      return l.letter + ArabicToRoman(i - l.value);
-      break;
+  const romanLetter = romanLetters[LetterIndex]
+    console.log(`remiander: ${input} , letter: ${romanLetter.letter} `)
+    if (input >= romanLetter.value) {
+      return romanLetter.letter + ArabicToRoman(input - romanLetter.value, LetterIndex);
+    } else if (LetterIndex < romanLetters.length) {
+      return ArabicToRoman(input, LetterIndex + 1);
     }
-  }
 } 
 
 
